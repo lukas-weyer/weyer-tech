4dceba53dcf66091bccb906a19587ed38e1d6fb0
Łukasz Weyer <weyer.rm@gmail.com>
2026-03-11 12:52:27 +0100

    feat: remove unused components, simplify page structure

--- app/page.js
+++ app/page.js
@@ -1,5 +1,3 @@
-import AboutCard from './_components/aboutCard';
-import ChatWindow from './_components/chatWindow';
 import Hero from './_components/hero';
 import Projects from './_components/projects';
 
@@ -7,8 +5,6 @@ export default function Home() {
   return (
     <main className="min-h-screen">
       <Hero />
-      <ChatWindow />
-      <AboutCard />
       <Projects />
     </main>
   );
